e253cf185c3f008f053d822e2d8cc2e6e85cc3ea
Josh Archer <jarcher@greenriver.edu>
2018-07-15 15:02:57 -0700

    Added several examples of loops in JavaScript

--- loops/loops.js
+++ loops/loops.js
@@ -1,41 +1,90 @@
-//principle #1: for loops
-//****************************************************
-
-//basic format
-var n = 100;
+//#1: using for (counting) loops
+var n = 10;
 for (var i = 1; i <= n; i++)
 {
-  //do something
+  //do something...
 }
 
-//flip a coin n times
-flipCoins(5);
-
-function flipCoins(times)
+//what sequence will the following loop create?
+for (var i = 3; i <= 8; i += 2)
 {
-  for (var i = 1; i <= times; i++)
+  document.write(i + " ");
+}
+document.write("<br>");
+
+var result = factorial(3);
+document.write(result + "<br>");
+
+result = factorial(4);
+document.write(result + "<br>");
+
+result = factorial(100);
+document.write(result + "<br>");
+
+result = factorial(1);
+document.write(result + "<br>");
+
+result = factorial(0);
+document.write(result + "<br>");
+
+//a function to calculate n!
+function factorial(num)
+{
+  var product = 1;
+  
+  for (var i = 1; i <= num; i++)
   {
-    var result = flipCoin();
-    if (result)
+    product *= i;
+  }
+  
+  return product;
+}
+
+document.write("1: " + prime(1) + "<br>");
+document.write("2: " + prime(2) + "<br>");
+document.write("3: " + prime(3) + "<br>");
+document.write("5: " + prime(5) + "<br>");
+document.write("6: " + prime(6) + "<br>");
+document.write("11: " + prime(11) + "<br>");
+document.write("1091: " + prime(1091) + "<br>");
+document.write("11111: " + prime(11111) + "<br>");
+
+function prime(num)
+{
+  //check for obvious non-prime values
+  if (num <= 1)
+  {
+    return false;
+  }
+  
+  //loop over lower numbers 
+  for (var i = 2; i < num; i++)
+  {
+    //see if num is evenly divisible by i
+    if (num % i === 0)
     {
-      document.write("Heads!<br>");
-    }
-    else
-    {
-      document.write("Tails!<br>");
+      return false; //not prime!
     }
   }
+  
+  return true; //this is prime!
 }
 
-//returns true for heads or false for tails
-function flipCoin()
+//how would you produce this sequence using a for loop?
+//-11, -14, -17, -20, -23, -26
+for (var i = -11; i >= -26; i -= 3)
 {
-  var coin = Math.floor(Math.random() * 2); //0 or 1
-  return coin == 1;
+  document.write(i + " ");
 }
+document.write("<br>");
 
-//principle #2: while loops
-//****************************************************
+//this is an infinite loop!
+/*for (;;)
+{
+  document.write("hello<br>");
+}*/
+
+//#2: using while loops
 
 var condition = true;
 while (condition)
@@ -45,67 +94,93 @@ while (condition)
   condition = false; //stop the loop
 }
 
-//while
+//ask the user for numbers until the sum >= 100
 var sum = 0;
-var output = "0";
-while (sum <= 100)
+while (sum < 100)
 {
-  //get a random value
-  var randomValue = Math.random() * 10; //0-9.999...
-  
-  //update sum and output
-  sum += randomValue;
-  output += " + " + randomValue.toFixed(2);
+  var userChoice = parseInt(prompt("Number?"));
+  sum += userChoice;
 }
-document.write(output + " = " + sum.toFixed(2) + "<br>");
+document.write("The sum was " + sum + "<br>");
 
-//principle #3: do-while loops
-//****************************************************
+//#3: using do-while loops
 
-var number;
-var finished = false;
+var favNumber;
+var likedNumber = false;
 do 
 {
-  number = prompt("Enter your favorite number");
-  finished = confirm("Are you sure?");
-} while (!finished);
-document.write("Your favorite number is: " + number + "<br>");
+  favNumber = parseInt(prompt("Fav number?"));
+  likedNumber = confirm("Are you sure?");
+} while (!likedNumber);
+document.write("Your favorite number is " + favNumber + "<br>");
 
-//principle #4: break, continue
-//****************************************************
+//#4: using break and continue
 
-//print numbers from 1-20 except when divisible by 7 or 10
-var output = "0";
+//print the numbers between 1 and 20 unless divisible by
+//3 or by 7
 for (var i = 1; i <= 20; i++)
 {
-  if (i % 7 == 0 || i % 10 == 0)
+  //detect bad values for i
+  if (i % 3 == 0 || i % 7 == 0)
   {
+    //move back to the top of the loop
     continue;
   }
-  output += ", " + i;
+  document.write(i + "<br>");
 }
-document.write(output + "<br>");
 
-//flip coins until 4 heads up come up
+for (var i = 1; i <= 10; i++)
+{
+  for (var j = 1; j <= 10; j++)
+  {
+    if (i == 3 || i == 5)
+    {
+      continue;
+    }
+    
+    document.write(j + " ");
+  }
+  document.write("<br>");
+}
+
+//flip coins until 4 heads come up in a row
 var heads = 0;
-var output = "";
 while (true)
 {
+  //get a "heads" or "tails" from flipCoin()
   var coin = flipCoin();
-  if (coin == true)
+  
+  //keep track of what we've seen
+  if (coin === "heads")
   {
-    heads++;
-    output += " heads";
+     heads++;
+  }
+  else 
+  {
+    heads = 0;
+  }
+  
+  //print out the flip
+  document.write(coin + "<br>");
+  
+  //are we done?
+  if (heads === 4)
+  {
+    break; //exit the loop
+  }
+}
+
+function flipCoin()
+{
+  //random() will produce a value between [0, 1)
+  var value = Math.floor(Math.random() * 2);
+  
+  if (value === 0)
+  {
+    return "heads";
   }
   else
   {
-    heads = 0;
-    output += " tails<br>";
-  }
-  
-  if (heads == 4)
-  {
-    break;
+    return "tails";
   }
 }
-document.write(output + "<br>");
