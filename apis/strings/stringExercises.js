e23aa41928b048b7bdd25928de9c124aa723c100
Josh Archer <jarcher@greenriver.edu>
2018-07-17 11:33:57 -0700

    Added an example of string parsing.

---
+++ apis/strings/stringExercises.js
@@ -0,0 +1,30 @@
+var source = "the dog said: a doggy dog world";
+var search = "dog";
+var results = find(source, search);
+document.write("dog found " + results + " times");
+
+function find(source, search)
+{
+  var count = 0;
+  var done = false;
+  
+  while (!done)
+  {
+    var foundIndex = source.indexOf(search);
+    
+    if (foundIndex != -1) //found it!
+    {
+      count++;
+      
+      //change the string to the rest of the string
+      var nextIndex = foundIndex + search.length;
+      source = source.substring(nextIndex);
+    }
+    else
+    {
+      done = true; //didn't find it!
+    }
+  }
+  
+  return count;
+}
