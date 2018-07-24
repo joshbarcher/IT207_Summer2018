b5d5768d6d86ccbfdad8a24002f3a5c5a2add65c
Josh Archer <jarcher@greenriver.edu>
2018-07-23 19:50:49 -0700

    Add examples of higher order functions with JavaScript arrays.

--- arrays/arraysPractice.js
+++ arrays/arraysPractice.js
@@ -44,12 +44,140 @@ nameParts.forEach(function(element) {
   console.log(element.toUpperCase());
 });
 
-var location = [1, 2, 3];
-console.log(location);
+//#4: use arrays to pass any number of arguments to a function
+//**********************************************
+
+function printMe()
+{
+  for (var i = 0; i < arguments.length; i++)
+  {
+    console.log(arguments[i]);
+    console.log(typeof arguments[i]);
+  }
+  console.log("printMe() finished");
+}
+
+//a dummy object
+var nameObject = {
+  fname: "Josh",
+  lname: "Archer"
+};
+
+//invoke the function with different parameters
+printMe();
+printMe("Josh", "was" ,"here");
+printMe(100, true, nameObject);
+
+var favorite = "true"; 
+if (favorite !== true) 
+{ 
+  alert("not equal"); 
+} 
+else 
+{ 
+  alert ("equal"); 
+}
+
+//#5: push() and pop()
+//**********************************************
+
+//add a few extra elements to our address
+address.push("beige");
+address.push("sq. ft. 1600");
+
+console.log(address);
+
+console.log(address.pop());
+console.log(address);
+
+//remove all elements in the array
+//address = [];
+
+while (address.length > 0) 
+{
+  address.pop();
+}
+
+console.log(address);
+
+//#6: splice() and slice()
+//**********************************************
+
+var letters = ["a", "b", "c"];
+
+//1 = index to add at, 0 = the number of elements to replace
+letters.splice(1, 0, "d", "e", "f"); 
+console.log(letters);
+
+var lessLetters = letters.slice(2, 4);
+console.log(lessLetters);
+
+//#7: map() and reduce()
+//**********************************************
+
+var nums = [1, 3, 4, 10, 15, 16, 20];
+nums = nums.map(function(element) {
+  return element ** 2;
+});
+
+console.log(nums);
+
+var sum = nums.reduce(function (total, element) {
+  return total + element;
+}, 0);
+
+console.log(sum);
+
+//#8: some() and every()
+//**********************************************
+
+var check = [1, 2, 1];
+
+//true if every element returns true with the given function
+var every = check.every(function(value, index, array) {
+  return value > 0;
+});
+
+console.log("Every: " + every);
+
+var some = check.some(function(value, index, array) {
+  return value === 3;
+});
+
+console.log("Some: " + some);
 
 
+//misc
+//**********************************************
 
+//storing functions in arrays
+var foo = 10;
+var functions = [addOne, addTwo, addThree];
+for (var i = 0; i < functions.length; i++)
+{
+  foo = functions[i](foo);
+}
 
+console.log(foo);
 
+function addOne(value)
+{
+  return value + 1;
+}
 
+function addTwo(value)
+{
+  return value + 2;
+}
 
+function addThree(value)
+{
+  return value + 3;
+}
+
+//invoking a function anonymously to avoid polluting the
+//global namespace
+(function() { 
+  var fname = "Juan";
+  alert("hello world, " + fname); 
+})();
