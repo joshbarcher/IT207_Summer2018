2e55cdbd08443026f004c9c25c1c1052abc9d3cd
Josh Archer <jarcher@greenriver.edu>
2018-08-06 11:31:18 -0700

    Added an example of forms in HTML and form-DOM interactions.

---
+++ dom/formHandling.js
@@ -0,0 +1,46 @@
+window.onload = function() {
+  document.querySelector("button").onclick = function(event) {
+    //this will stop the default behavior (form submitting)
+    event.preventDefault();
+    
+    //most control values can be accessed using the value attribute
+    var name = document.querySelector('input[name="full-name"]').value;
+    var state = document.querySelector('select[name="state"]').value;
+    
+    console.log(name);
+    console.log(state);
+    
+    //determine which radio button is selected...
+    console.log("Gender: " + getRadioValue("gender"));
+    
+    var boxes = document.querySelectorAll('input[type="checkbox"]');
+    for (var i = 0; i < boxes.length; i++)
+    {
+      if (boxes[i].checked)
+      {
+        console.log(boxes[i].name + ": " + boxes[i].value);
+      }
+    }
+  };
+};
+
+function getRadioValue(name)
+{
+  var radios = document.querySelector('input[name="' + name + '"]:checked');
+  return radios.value;
+  /*var value = "";
+  for (var i = 0; i < radios.length; i++)
+  {
+    if (radios[i].checked)
+    {
+      value = radios[i].value;
+    }
+  }
+  return value;*/
+}
+
+
+
+
+
+
