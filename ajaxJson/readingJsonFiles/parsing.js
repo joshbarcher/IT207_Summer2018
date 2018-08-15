c7210bce874a9f4eae635b0fe12954e0fc966abd
Josh Archer <jarcher@greenriver.edu>
2018-08-15 10:36:54 -0700

    Added an example of reading a JSON file over Ajax.

--- ajaxJson/readingJsonFiles/parsing.js
+++ ajaxJson/readingJsonFiles/parsing.js
@@ -22,4 +22,34 @@ function loadClub()
 function loadUI(data)
 {
   $("h1").html(data.title);
+  
+  //print out meeting times
+  var times = data["meeting times"];
+  var output = "";
+  for (var day in times)
+  {
+    var meetingOnDay = times[day];
+    if (meetingOnDay)
+    {
+      output += "We are meeting on " + day + "<br>";
+    }
+  }
+  
+  $("p#meeting-times").html(output);
+  
+  var membersList = $("ul#members");
+  for (var i = 0; i < data.members.length; i++)
+  {
+    membersList.append("<li>" + data.members[i] + "</li>");
+  }
 }
+
+
+
+
+
+
+
+
+
+
